8bd74bf4b6d19a56f6322882f2b2685a0fb14fdb
willu2 <bob@example.com>
2019-04-04 16:47:26 +0300

    version 1

--- src/main/java/MarketCreator.java
+++ src/main/java/MarketCreator.java
@@ -1,7 +1,36 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class MarketCreator {
 
-    String[] stonesArray = {"Diamond", "Ruby", "Sapphire", "Emerald"};
+    String[] stonesArray = {"Diamond", "Ruby", "Sapphire", "Emerald", "Amazonite",
+            "Pietersite", "Amethyst", "Rubellite", "Hessonite", "Charoite"};
+
+    ArrayList <StoneItem> stouncDB = new ArrayList<>();
+
+    StoneItem stoneItem;
+    private int tempPrice = 0;
 
     public MarketCreator() {
+    }
+
+    public String randStoneName(String[] stArray){
+        Random random = new Random();
+        int index = random.nextInt(stArray.length - 1);
+        String stone = stonesArray[index];
+        return stone;
+    }
+
+    public int randStoneMass(){
+        Random random = new Random();
+        int mass = random.nextInt(100) + 12;
+        tempPrice = mass;
+        return mass;
+    }
+
+    public int randStonePrice(){
+        Random random = new Random();
+        int price = random.nextInt(20000) + tempPrice/2;
+        return price;
     }
 }
